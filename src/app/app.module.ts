c6e3887cc22c3838e3c1654babc7254089210a16
MolSnoo <molsno.vivian@gmail.com>
2022-03-22 10:55:48 -0400

    Added fullcalendar component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuitemComponent } from '../components/menuitem/menuitem.component';
 import { CheckoutComponent } from '../components/checkout/checkout.component';
 import { HeaderComponent } from '../components/header/header.component';
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [
@@ -16,7 +24,8 @@ import { HeaderComponent } from '../components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
