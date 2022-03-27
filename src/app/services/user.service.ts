34ea45e880faa1b3fd12c04ae33838019c0929a9
michaelbutera <michael213@revature.net>
2022-03-27 16:10:18 -0400

    Made progress with login validation

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { User } from "../classes/user";
 
@@ -20,22 +20,37 @@ export class UserService {
         //var email = this.http.get<User>('http://localhost:9000/users').email;
         var email = user.email;
         var password = user.password;
+        var passedemail = "";
+        var passedpassword = "";
         // if(email==user.email && password==this.http.get<User>('http://localhost:9000/users')) {
         // if(email==user.email && password==this.getValidation(user)) {
-        if(email==user.email && password==user.password) {
+        
+        //changing params
+        let params = new HttpParams().set('email', email,);
+        passedemail = this.http.get<User>(`http://localhost:9000/login`, { params : params });
+        params = new HttpParams().set('password', password,);
+        passedpassword = this.http.get<User>(`http://localhost:9000/login`, { params : params });
+
+        if(email==passedemail && password==passedpassword) {
             console.log("login successful");
             alert("Login successful");
         } else {
             console.log("Invalid email or password");
             alert("Invalid email or password");
         }
-        return this.http.get<User>('http://localhost:9000/users');
-        // user.email
-        //return this.http.get<User>('http://localhost:9000/users');
+        // return this.http.get<User>('http://localhost:9000/login');
+
+        // both params in 1
+        // let params = new HttpParams();
+        // params = params.append('email', email);
+        // params = params.append('password', password);
+
+        return this.http.get<User>(`http://localhost:9000/login`, { params : params });
+        // return this.http.get<User>('http://localhost:9000/users');
         // return this.http.get<User>('http://localhost:9000/users', {responseType: "text"});
     }
 
     getValidation(user : User) {
-        return this.http.get<User>('http://localhost:9000/users');
+        return this.http.get<User>('http://localhost:9000/login');
     }
 }
